84460317aa9f5222e906c15d7433933f93af3715
BenG49 <ben@goldfisher.com>
2022-07-20 17:50:54 -0400

    ppu stuffs

--- include/ppu.hpp
+++ include/ppu.hpp
@@ -1,15 +1,41 @@
 #pragma once
 
-/*
+#include <util.hpp>
 
-PPU:
-- 256x240
-	- 64 sprites
-	- scrollable field
-
-*/
+const uint8_t PPUCTRL = 0;
+const uint8_t PPUMASK = 1;
+const uint8_t PPUSTATUS = 2;
+const uint8_t OAMADDR = 3;
+const uint8_t OAMDATA = 4;
+const uint8_t PPUSCROLL = 5;
+const uint8_t PPUADDR = 6;
+const uint8_t PPUDATA = 7;
 
 class PPU {
+private:
+	/*
+	Pattern table defines the shapes of tiles and sprites
+	and is split into two parts - left and right
+
+	Each tile is 16 bytes - two 8 byte bit planes, low
+	and high.
+
+	Located from $0000 - $1FFF
+	*/
+	uint8_t patterntable[0x2000];
+	uint8_t nametable[0x1000];
+	uint8_t palette[0x20];
+
+	struct Addr {
+		uint16_t addr;
+		bool wrote_hi;
+	};
+
+	Addr ppuaddr;
+
+	uint8_t ppu_read(uint8_t addr);
+	uint8_t ppu_write(uint8_t addr, uint8_t val);
+
 public:
 	enum Mirroring {
 		VERTICAL,
@@ -18,4 +44,7 @@ public:
 	};
 
 	PPU();
+
+	uint8_t read(uint8_t addr);
+	uint8_t write(uint8_t addr, uint8_t val);
 };
--- src/ppu.cpp
+++ src/ppu.cpp
@@ -1,2 +1,32 @@
 #include <ppu.hpp>
 
+uint8_t PPU::ppu_read(uint8_t addr) {
+	if (addr < 0x2000) {
+		// uint8_t 
+	}
+}
+
+uint8_t PPU::read(uint8_t addr) {
+	if (addr >= 0x2000 && addr < 0x4000) {
+		addr &= 0b111;
+	}
+}
+
+uint8_t PPU::write(uint8_t addr, uint8_t val) {
+	if (addr >= 0x2000 && addr < 0x4000) {
+		addr &= 0b111;
+
+		// PPUADDR
+		if (addr == 6) {
+			if (ppuaddr.wrote_hi) {
+				ppuaddr.addr &= 0xFF00;
+				ppuaddr.addr |= val;
+			} else {
+				ppuaddr.addr &= 0xFF;
+				ppuaddr.addr |= (val << 8);
+
+				ppuaddr.wrote_hi = true;
+			}
+		}
+	}
+}
